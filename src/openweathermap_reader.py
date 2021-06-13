681cae736f4bf60f20155d5745d601e56ee4c23f
AoWangPhilly <aowangphilly@gmail.com>
2021-06-13 16:18:54 -0400

    Rename class

--- src/openweathermap_reader.py
+++ src/openweathermap_reader.py
@@ -14,7 +14,7 @@ UNITS = 'metric'
 QUERY_URL = f'{BASE_URL}q={CITY_NAME}&appId={TOKEN}&units={UNITS}'
 # ----------------------------------------------------------------------
 
-class OpenWeatherAppReader:
+class OpenWeatherMapReader:
 	def __init__(self, url_query):
 		self.url_query = url_query
 	
@@ -25,12 +25,12 @@ class OpenWeatherAppReader:
 			main = data['main']
 			temp, temp_min, temp_max, humidity, time = main['temp'], main['temp_min'], main['temp_max'], main['humidity'], data['dt']
 			time = dt.datetime.fromtimestamp(int(time)).isoformat()
-			return time, temp, temp_min, temp_max, humidity
+			return time, humidity, temp, temp_min, temp_max
 
 
 		
 if __name__ == '__main__':
-	weather_api = OpenWeatherAppReader(url_query=QUERY_URL) 
+	weather_api = OpenWeatherMapReader(url_query=QUERY_URL) 
 	print(weather_api.get_temperature_and_humidity())
 
 
