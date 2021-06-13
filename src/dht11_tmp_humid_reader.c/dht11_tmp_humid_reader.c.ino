20bf9ab6e5368c7e056540a02e37a8463a10ce3a
AoWangPhilly <aowangphilly@gmail.com>
2021-06-13 11:38:54 -0400

    Move to src folder

---
+++ src/dht11_tmp_humid_reader.c/dht11_tmp_humid_reader.c.ino
@@ -0,0 +1,32 @@
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define TYPE DHT11
+
+int sensePin = 2;
+DHT HT(sensePin, TYPE);
+
+float humidity;
+float tempC;
+float tempF;
+
+// delay a minute between each read
+unsigned long setTime = 1*60*1000UL;
+
+void setup() {
+  Serial.begin(9600);
+  HT.begin();
+}
+
+void loop() {
+  humidity = HT.readHumidity();
+  tempC = HT.readTemperature();
+
+  if (!isnan(humidity) && !isnan(tempC)){
+    Serial.print(humidity);
+    Serial.print("\t");
+    Serial.print(tempC);
+    Serial.println();
+  }
+  delay(setTime);
+}
---
+++ src/read_serial.py
@@ -0,0 +1,38 @@
+import serial
+import sqlite3
+import datetime as dt
+
+# Serial port: ls -l /dev/cu.usb*
+SERIAL_PORT = '/dev/cu.usbmodem2201'
+
+conn = sqlite3.connect('../sqlite/customer.db')
+cursor = conn.cursor()
+
+ser = serial.Serial(SERIAL_PORT)
+ser.flushInput()
+
+
+while True:
+	try:	
+		# Read line and decode to string
+		ser_bytes = ser.readline().decode('utf-8')
+		
+		# Separate the string by \t and turn to floats
+		# [humidity, temperature (in C)
+		decoded_bytes = list(map(float, ser_bytes.split('\t')))
+		
+		time_read = dt.datetime.now().isoformat()
+		humidity, temperature = decoded_bytes[0], decoded_bytes[1]
+		
+		cursor.execute('INSERT INTO room_metrics VALUES (?, ?, ?)', time_read, humidity, temperature)
+		conn.commit()
+	except:
+		print("Something went wrong?")
+		break
+
+# Close out connection
+conn.close() 
+		
+		
+
+
