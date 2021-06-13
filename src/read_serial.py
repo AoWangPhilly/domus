80c8743351ccc247b3fd205d8a9fa7bec0c22e44
AoWangPhilly <aowangphilly@gmail.com>
2021-06-13 11:44:37 -0400

    Redirect to correct db

--- src/read_serial.py
+++ src/read_serial.py
@@ -5,7 +5,7 @@ import datetime as dt
 # Serial port: ls -l /dev/cu.usb*
 SERIAL_PORT = '/dev/cu.usbmodem2201'
 
-conn = sqlite3.connect('../sqlite/customer.db')
+conn = sqlite3.connect('../sqlite/temperature_and_humidity.db')
 cursor = conn.cursor()
 
 ser = serial.Serial(SERIAL_PORT)
@@ -13,22 +13,18 @@ ser.flushInput()
 
 
 while True:
-	try:	
-		# Read line and decode to string
-		ser_bytes = ser.readline().decode('utf-8')
+	# Read line and decode to string
+	ser_bytes = ser.readline().decode('utf-8')
 		
-		# Separate the string by \t and turn to floats
-		# [humidity, temperature (in C)
-		decoded_bytes = list(map(float, ser_bytes.split('\t')))
+	# Separate the string by \t and turn to floats
+	# [humidity, temperature (in C)
+	decoded_bytes = list(map(float, ser_bytes.split('\t')))
 		
-		time_read = dt.datetime.now().isoformat()
-		humidity, temperature = decoded_bytes[0], decoded_bytes[1]
+	time_read = dt.datetime.now().isoformat()
+	humidity, temperature = decoded_bytes[0], decoded_bytes[1]
 		
-		cursor.execute('INSERT INTO room_metrics VALUES (?, ?, ?)', time_read, humidity, temperature)
-		conn.commit()
-	except:
-		print("Something went wrong?")
-		break
+	cursor.execute('INSERT INTO room_metrics VALUES (?, ?, ?)', (time_read, humidity, temperature))
+	conn.commit()
 
 # Close out connection
 conn.close() 
