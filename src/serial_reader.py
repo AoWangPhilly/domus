4d049911259b8f9f1a2ac34eff0796d678b3b20f
AoWangPhilly <aowangphilly@gmail.com>
2021-06-16 22:00:01 -0400

    Add in typing

--- src/serial_reader.py
+++ src/serial_reader.py
@@ -1,5 +1,6 @@
 import serial
 import datetime as dt
+from typing import List
 
 # Serial port: ls -l /dev/cu.usb*
 SERIAL_PORT = '/dev/cu.usbmodem2201'
@@ -9,12 +10,12 @@ class SerialReader:
 		self.ser = serial.Serial(port)
 		self.ser.flushInput()
 		
-	def read_serial(self):
+	def read_serial(self) -> List[str]:
 		# Read line and decode to string
 		ser_bytes = self.ser.readline().decode('utf-8')
 		return self.__clean_serial_output(ser_bytes)
 	
-	def __clean_serial_output(self, output):
+	def __clean_serial_output(self, output: str) -> List[str]:
 		# Separate the string by tab and turn into floats
 		# [humidity, temperature (C)]
 		decoded_bytes = list(map(float, output.split('\t')))
